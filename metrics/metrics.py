a3df4a1732f1b2aeed253c6966e3b24735fce966
Jack Lee <jack.lyhn.lee@gmail.com>
2022-01-23 01:22:08 -0800

    Fixed metrics to match SetVAE implementation

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 from tqdm import tqdm
 
 # Import CUDA version of approximate EMD, from https://github.com/zekunhao1995/pcgan-pytorch/
@@ -8,60 +7,48 @@ from metrics.StructuralLosses.nn_distance import nn_distance
 
 
 def compute_cd(x, y, reduce_func=torch.mean):
-    if x.is_cuda:
-        d1, d2 = nn_distance(x, y)
-    else:
-        bs, num_points, points_dim = x.size()
-        xx = torch.bmm(x, x.transpose(2, 1))
-        yy = torch.bmm(y, y.transpose(2, 1))
-        zz = torch.bmm(x, y.transpose(2, 1))
-        diag_ind = torch.arange(0, num_points).to(sample).long()
-        rx = xx[:, diag_ind, diag_ind].unsqueeze(1).expand_as(xx)
-        ry = yy[:, diag_ind, diag_ind].unsqueeze(1).expand_as(yy)
-        p = rx.transpose(2, 1) + ry - 2 * zz
-        d1, d2 = p.min(1)[0], p.min(2)[0]
+    d1, d2 = nn_distance(x, y)
     return reduce_func(d1, dim=1) + reduce_func(d2, dim=1)
 
 
 def compute_emd(x, y):
-    return match_cost(x, y) / float(x.size(1))
+    return match_cost(x, y) / x.size(1)
 
 
 def compute_pairwise_cd_emd(x, y, batch_size=32):
-    cd, emd = [], []
+    NX, NY, cd, emd = x.size(0), y.size(0), [], []
     y = y.contiguous()
-    for xi in tqdm(x):
-        cdi, emdi = [], []
-        for yb in torch.split(y, batch_size, dim=0):
+    for i in tqdm(range(NX)):
+        cdx, emdx, xi = [], [], x[i]
+        for j in range(0, NY, batch_size):
+            yb = y[j : j + batch_size]
             xb = xi.view(1, -1, 3).expand_as(yb).contiguous()
-            cdi.append(compute_cd(xb, yb).view(1, -1))
-            emdi.append(compute_emd(xb, yb).view(1, -1))
-        cd.append(torch.cat(cdi, dim=1))
-        emd.append(torch.cat(emdi, dim=1))
+            cdx.append(compute_cd(xb, yb).view(1, -1))
+            emdx.append(compute_emd(xb, yb).view(1, -1))
+        cd.append(torch.cat(cdx, dim=1))
+        emd.append(torch.cat(emdx, dim=1))
     cd, emd = torch.cat(cd, dim=0), torch.cat(emd, dim=0)
     return cd, emd
 
 
-def compute_mmd_cov(d):
-    NS, NR = d.shape
-    min_val_fromsmp, min_idx = d.min(dim=1)
-    min_val, _ = d.min(dim=0)
+def compute_mmd_cov(dxy):
+    _, min_idx = dxy.min(dim=1)
+    min_val, _ = dxy.min(dim=0)
     mmd = min_val.mean()
-    cov = torch.tensor(min_idx.unique().view(-1).size(0) / float(NR)).to(d)
+    cov = min_idx.unique().numel() / dxy.size(1)
+    cov = torch.tensor(cov).to(dxy)
     return mmd, cov
 
 
-def compute_knn(dxx, dxy, dyy, k):
-    X, Y = dxx.size(0), dyy.size(0)
-    label = torch.cat((torch.ones(X), torch.zeros(Y))).to(dxx)
-    mx = torch.cat((dxx, dxy), dim=1)
-    my = torch.cat((dxy.t(), dyy), dim=1)
-    m = torch.cat((mx, my), dim=0)
-    m = m + torch.diag(torch.ones(X + Y) * float("inf")).to(dxx)
+def compute_knn(dyy, dyx, dxx, k):
+    X, Y = torch.ones(dxx.size(0)), torch.zeros(dyy.size(0))
+    XY = torch.ones(dxx.size(0) + dyy.size(0)).to(dxx)
+    lb = torch.cat((Y, X)).to(dxx)
+    my = torch.cat((dyy, dyx), dim=1)
+    mx = torch.cat((dyx.t(), dxx), dim=1)
+    m = torch.cat((my, mx), dim=0)
+    m = m + torch.diag(XY * float("inf"))
     _, idx = m.topk(k, dim=0, largest=False)
-    count = torch.zeros(X + Y).to(dxx)
-    for i in range(0, k):
-        count = count + label.index_select(0, idx[i])
-    pred = torch.ge(count, torch.ones(X + Y).to(dxx) * (float(k) / 2)).float()
-    acc = torch.eq(label, pred).float().mean()
+    count = sum(lb.index_select(0, idx[i]) for i in range(k))
+    acc = (lb == (count >= (XY * (k / 2)))).float().mean()
     return acc
